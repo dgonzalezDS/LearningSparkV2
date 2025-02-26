df0c2f09d63d44583ccd58a35f917ecfe1f7a15b
David <david.gternero@bosonit.com>
2025-02-26 08:59:03 +0100

    test añadido

--- src/main/scala/chapter5.scala
+++ src/main/scala/chapter5.scala
@@ -1,13 +1,19 @@
 import org.apache.spark.sql.{SparkSession, DataFrame}
 
-
-
+/*
+*   Este tema trata sobre la conexion de fuentes de datos externas, como bases de datos de Postgre o MS SQL
+*   Para ejecutar correctamente el capitulo, además de pasar por parámetros de ejecución el numero de capitulo y el ejercicio,
+*   es necesario pasar por "variables de entorno"  las credenciales de acceso a las diferentes bases de datos, ocultas por motivos de seguridad.
+*
+*   Nota: Cada conexión es distinta, pero en la mayoría (o todas) es necesario especificar el nombre de la base de datos y el nombre de la tabla que queremos conectar
+*  */
+probando cosas
 object chapter5 {
   def ejercicio1(spark: SparkSession): Unit = {
     /* Conexion con POSTGRESDB */
 
     // Parámetros de conexión
-    val url = "jdbc:postgresql://localhost:5432/OLTP"
+    val url = "jdbc:postgresql://localhost:5432/OLTP"  // El nombre de la base de datos es OLTP (postgre usa por defecto el puerto 5432)
     val properties = new java.util.Properties()
 
     // Obtener usuario y contraseña de manera segura
