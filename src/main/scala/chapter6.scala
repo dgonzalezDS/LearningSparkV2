8bc020a887fcef003d6e5119f604ee55ed753bf5
dgonzalezDS <dvdgonzalez12@gmail.com>
2025-02-26 13:24:46 +0100

    Inicio del capitulo 6

---
+++ src/main/scala/chapter6.scala
@@ -0,0 +1,9 @@
+import org.apache.spark.sql.{SparkSession, DataFrame}
+import java.util.Properties
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+
+object chapter6 {
+  def ejercicio1(spark: SparkSession): Unit = {
+
+  }
